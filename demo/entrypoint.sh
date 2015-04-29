da924e6fa2da99faa7168d538d8716c3aad50824
Julien Fastré <julien.fastre@champs-libres.coop>
2015-04-29 23:11:07 +0200

    improve config on startup
    some composer script have to be removed from composer.json
    in entry.sh, the first command is run under www-data => the cache is build with www-data

--- demo/entrypoint.sh
+++ demo/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-/usr/bin/php /var/www/chill/app/console doctrine:migrations:migrate --no-interaction
-/usr/bin/php /var/www/chill/app/console assetic:dump
-/usr/bin/php /var/www/chill/app/console assets:install
+sudo -u www-data /usr/bin/php /var/www/chill/app/console cache:clear --env=prod
+/usr/bin/php /var/www/chill/app/console doctrine:migrations:migrate --no-interaction --env=prod
+/usr/bin/php /var/www/chill/app/console assetic:dump --env=prod
+/usr/bin/php /var/www/chill/app/console assets:install --env=prod
 /usr/sbin/apache2ctl -D FOREGROUND

1270e2d35013bd4b72d16bd2513ae8827657e5c4
Julien Fastré <julien.fastre@champs-libres.coop>
2015-05-04 22:38:31 +0200

    seperation of apache/php-fpm/nginx

--- demo/entrypoint.sh
+++ demo/entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+#immediatly exit if a command fails:
+set -e
+
+#prepare cache - executed as www-data
 sudo -u www-data /usr/bin/php /var/www/chill/app/console cache:clear --env=prod
+
+#migrate
 /usr/bin/php /var/www/chill/app/console doctrine:migrations:migrate --no-interaction --env=prod
+
+#dump file
 /usr/bin/php /var/www/chill/app/console assetic:dump --env=prod
 /usr/bin/php /var/www/chill/app/console assets:install --env=prod
+
+#start apache
 /usr/sbin/apache2ctl -D FOREGROUND
